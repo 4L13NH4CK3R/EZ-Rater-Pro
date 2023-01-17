41cae295dfb2424eeada0bfd4cd32893126aa33d
CryptoH4ck3r <crytptoh4ck3r@proton.me>
2023-01-16 22:03:41 -0600

    Added SSL & PUMA Web Server! Setup User Registration Process

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+  validates :username, presence: true, length: { maximum: 20 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
+  validates :first_name, presence: true, length: { maximum: 20 }
+  validates :last_name, presence: true, length: { maximum: 20 }
+  validates :access_code, presence: true, length: { maximum: 3 }
+  validates :agency_name, presence: true, length: { maximum: 30 }
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
+end

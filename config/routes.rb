586543f940c9da11d96a7b2fd7fb08727fb41a1c
CryptoH4ck3r <crytptoh4ck3r@proton.me>
2023-01-16 22:34:08 -0600

    Added Login Features to the Application

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,11 @@ Rails.application.routes.draw do
 
   # Set Users Paths #
   get '/signup',  to: 'users#new'
+  get    "/login",   to: "sessions#new"
+  post   "/login",   to: "sessions#create"
+  delete "/logout",  to: "sessions#destroy"
+
 
   # Set Project Resources #
-  resources :users 
+  resources :users
 end
